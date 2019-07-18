c27c37e0da39e2ba6b7cf7da296bffdf2ff5f7e2
Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
2019-07-18 12:19:49 +0200

    fp_vdev_remote: improve an arbitrary fp-rcpd.sock path support
    Add get_rpcd_sock() function in vdev_utils to better support an
    arbitrary fp-rcpd.sock path.
    
    1. if default fp-rcpd.sock path exists we just return its value.
    
    2. If default fp-rcpd.sock path doesn't exist, we try to get it from
    fp-vdev-remote.ini and return the value, if obtained from the config
    path is presented in the file system.
    
    3. If fp-vdev-remote.ini was not provided or doesn't contain
    'fp_rpcd_dir' setting, we perform lookup to '/run' directory and return
    the path value, if lookup was successfull. Otherwise FP_RPCD_SOCK
    default value will be returned.
    
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
    Acked-by: Maxime Leroy <maxime.leroy@6wind.com>

--- fp_vdev_remote/vdev_utils.py
+++ fp_vdev_remote/vdev_utils.py
@@ -12,6 +12,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import fnmatch
 import os
 import subprocess
 import ConfigParser
@@ -20,12 +21,11 @@ from distutils.spawn import find_executable
 
 _REL_CFG_PATH = '../../etc'
 _FPDEVRMT_INI = 'fp-vdev-remote.ini'
-FP_RPCD_DIR = '/var/run/fp_rpcd'
+FP_RPCD_SOCK = '/var/run/fp_rpcd/fp-rpcd.sock'
 FP_VDEV = 'fp-vdev'
 FP_VDEV_RMT = 'fp-vdev-remote'
-SOCKFILE = 'fp-rpcd.sock'
 
-
+#-------------------------------------------------------------------------------
 def get_config_path():
     fpvdevrmt_exec = find_executable('fp-vdev-remote')
     fpvdevrmt_path = os.path.join(os.path.dirname(fpvdevrmt_exec),
@@ -33,18 +33,22 @@ def get_config_path():
     fpvdevrmt_opts = os.path.join(fpvdevrmt_path, _FPDEVRMT_INI)
     return fpvdevrmt_opts
 
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 def get_conf():
     Config = ConfigParser.ConfigParser()
     cfg_path = get_config_path()
     Config.read(cfg_path)
-    cfg = {'FP_RPCD_DIR': FP_RPCD_DIR}
+    cfg = {'FP_RPCD_SOCK': FP_RPCD_SOCK}
     try:
-        cfg['FP_RPCD_DIR'] = Config.get('rpcd', 'fp_rpcd_dir')
+        fp_rpcd_dir = Config.get('rpcd', 'fp_rpcd_dir')
+        if fp_rpcd_dir:
+            cfg['FP_RPCD_SOCK'] = os.path.join(fp_rpcd_dir,
+                                               os.path.basename(FP_RPCD_SOCK))
     except:
-        # silently return default value if fp_rpcd_dir or fp-vdev-remote.ini
-        # were not found
+        # silently return default FP_RPCD_SOCK if fp_rpcd_dir or
+        # fp-vdev-remote.ini were not found
         pass
+
     return cfg
 
 # ------------------------------------------------------------------------------
@@ -64,3 +68,24 @@ def get_vdev_cmd():
         return FP_VDEV_RMT
     else:
         return None
+
+#-------------------------------------------------------------------------------
+def get_rpcd_sock():
+    # check if default/updated path exists
+    if os.path.exists(FP_RPCD_SOCK):
+        return FP_RPCD_SOCK
+    # try to update path from conf
+    cfg = get_conf()
+    sockpath = cfg.get('FP_RPCD_SOCK')
+    if os.path.exists(sockpath):
+        return sockpath
+    else:
+        # perform lookup to '/run',
+        # if you change FP_RPCD_SOCK default, please, take attention to the
+        # obtained dirs value, just not to preform lookup to '/'
+        dirs, sockname = os.path.split(FP_RPCD_SOCK)
+        for dir, subdir, filelist in os.walk(os.path.dirname(dirs)):
+            if [file for file in filelist if fnmatch.fnmatch(file, sockname)]:
+                return os.path.join(dir, sockname)
+
+    return FP_RPCD_SOCK
